c9533054d842825e0c3f2a371da61ce6c9483ae4
Eric.Kinch <qi.jin@happysports.com>
2015-01-19 17:56:39 +0800

    cart page.

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,17 +1,21 @@
-angular.module('cartApp', [])
-	.controller('cartController', ['$scope', function ($scope) {
-		
-		$scope.items = [
-			{price:"101", quantity: "2", title: "yy"},
-			{price:"102", quantity: "12", title: "yy3"},
-			{price:"103", quantity: "22", title: "yy2"},
-			{price:"104", quantity: "32", title: "yy1"}
-		];
+var items = [
+	{price:"101", quantity: "2", title: "yy"},
+	{price:"102", quantity: "12", title: "yy3"},
+	{price:"103", quantity: "22", title: "yy2"},
+	{price:"103", quantity: "22", title: "yy232"},
+	{price:"104", quantity: "32", title: "yy1"}
+];
 
-		$scope.remove = function(index){
-			$scope.items.splice(index, 1);
-		}
+var cartApp = angular.module('cartApp', []);
+cartApp.controller('cartController', ['$scope', function ($scope) {
+	
+	$scope.items = items;
+	$scope.remove = function(index){
+		$scope.items.splice(index, 1);
+	}
 
+}]);
 
-
-	}]);
+cartApp.controller('tController', ['$scope', function ($scope){
+	$scope.t_name = "Hello Kinch";
+}]);
