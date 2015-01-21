0e0e7d4535f47dca964e517b804cc80d94c7e390
Eric.Kinch <qi.jin@happysports.com>
2015-01-21 09:42:31 +0800

    toggle menu index for repeat

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,8 +1,6 @@
 var items = [
 	{price:"101", quantity: "2", title: "yy"},
 	{price:"102", quantity: "12", title: "yy3"},
-	{price:"103", quantity: "22", title: "yy2"},
-	{price:"103", quantity: "22", title: "yy232"},
 	{price:"104", quantity: "32", title: "yy1"}
 ];
 
@@ -14,8 +12,19 @@ cartApp.controller('cartController', ['$scope', function ($scope) {
 		$scope.items.splice(index, 1);
 	}
 
+	$scope.add = function(){
+		$scope.items.push({price:"114", quantity: "32", title: "Hello"});
+	}
+
 }]);
 
 cartApp.controller('tController', ['$scope', function ($scope){
+
 	$scope.t_name = "Hello Kinch";
+
+	$scope.menuState = false;
+
+	$scope.toggleMenu = function () {
+		$scope.menuState = ! $scope.menuState;
+	}
 }]);
