0097b7f05de41228dbeac9cc841b37800df6d87d
kinch <jinqiqiqi@gmail.com>
2015-06-10 21:49:36 +0800

    add admin orders page.

--- sportsstore/angularjs/controllers/adminController.js
+++ sportsstore/angularjs/controllers/adminController.js
@@ -1,1 +1,61 @@
-var app = angular.module("sportsStoreAdmin");
+var app = angular.module("sportsStoreAdmin", ["ngRoute"]);
+app
+.constant('authUrl', "http://192.168.1.7:5500/users/login")
+.constant('ordersUrl', "http://192.168.1.7:5500/orders")
+.controller('authCtrl', ['$scope', '$http', '$location', 'authUrl', function($scope, $http, $location, authUrl){
+
+	$scope.authenticate = function(user, pass) {
+		$http.post(authUrl, {
+			username: user,
+			password: pass
+		}, {
+			withCredentials: true
+		})
+		.success(function(data){
+			$location.path("/main");
+		})
+		.error(function(error) {
+			$scope.authenticationError = error;
+		});
+	}
+
+}])
+.controller('mainCtrl', ['$scope', function($scope){
+	
+	$scope.screens = ["Products", "Order"];
+	$scope.current = $scope.screens[0];
+
+	$scope.setScreen = function(index) {
+		$scope.current = $scope.screens[index];
+	}
+
+	$scope.getScreen = function() {
+		return $scope.current == "Products"? "/views/adminProducts.html": "/views/adminOrders.html";
+	}
+
+}])
+.controller('ordersCtrl', ['$scope', '$http', 'ordersUrl', function($scope, $http, ordersUrl){
+	
+	$http.get(ordersUrl, {withCredentials: true})
+	.success(function(data){
+		$scope.orders = data;
+	})
+	.error(function(error) {
+		$scope.error = error;
+	});
+
+	$scope.selectedOrder;
+	$scope.selectOrder = function(order) {
+		$scope.selectedOrder = order;
+	}
+
+	$scope.calcTotal = function(order) {
+		var total = 0;
+		for (var i = 0; i < order.products.length; i++) {
+			total += order.products[i].count * order.products[i].price;
+		};
+		return total;
+	}
+
+}])
+;
